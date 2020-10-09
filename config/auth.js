34c3368e0ae25593fa91266d0851dfa669b62e3a
Ethan Anderson <eanderson0813@protonmail.com>
2020-10-09 15:39:14 -0500

    Added Spotify creds

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CLIENT_ID: '<your_client_id>',
-  CLIENT_SECRET: '<your_client_secret>'
+  CLIENT_ID: 'e4c8787e4ee54b3782454c489e17a9c4',
+  CLIENT_SECRET: '66a7cdfdfc774b76b85b5d14bc35b5d3'
 };
